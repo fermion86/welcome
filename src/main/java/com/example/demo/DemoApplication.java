93204c8029408fa3aa60e897b9e13b93a0f8a57d
fermion86 <fermion86@daum.net>
2020-03-19 21:27:34 +0900

    성공이다 성공이야

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("정다기 작성 ");
+		System.out.println("성공이다 성공이야");
 	}
 
 }
